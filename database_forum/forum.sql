406fbd9891e4ba071d83f6d000e0c2c42466dd5f
Asma <148782301+yah422@users.noreply.github.com>
2024-04-17 11:58:01 +0200

    Update forum.sql

--- database_forum/forum.sql
+++ database_forum/forum.sql
@@ -72,6 +72,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 -- Listage des données de la table forum_asma.user : ~0 rows (environ)
 
+
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
