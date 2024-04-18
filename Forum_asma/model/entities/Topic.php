26a80ffa4bbde46b4ac2a37a851d2ab83f02b39f
Asma SAIDI <saidiasma422@gmail.com>
2024-04-18 10:04:20 +0200

    ADD COMMENT TO THE ENTITIES

--- Forum_asma/model/entities/Topic.php
+++ Forum_asma/model/entities/Topic.php
@@ -4,7 +4,9 @@ namespace Model\Entities;
 use App\Entity;
 
 /*
-    En programmation orientée objet, une classe finale (final class) est une classe que vous ne pouvez pas étendre, c'est-à-dire qu'aucune autre classe ne peut hériter de cette classe. En d'autres termes, une classe finale ne peut pas être utilisée comme classe parente.
+    En programmation orientée objet, une classe finale (final class) est une classe que vous ne pouvez pas étendre, 
+    c'est-à-dire qu'aucune autre classe ne peut hériter de cette classe. 
+    En d'autres termes, une classe finale ne peut pas être utilisée comme classe parente.
 */
 
 final class Topic extends Entity{
@@ -16,61 +18,39 @@ final class Topic extends Entity{
     private $creationDate;
     private $closed;
 
+    // METHODE CONSTRUCT DE TOPIC
     public function __construct($data){         
         $this->hydrate($data);        
     }
 
-    /**
-     * Get the value of id
-     */ 
+    // GET ET SET DE L'ID
     public function getId(){
         return $this->id;
     }
-
-    /**
-     * Set the value of id
-     *
-     * @return  self
-     */ 
     public function setId($id){
         $this->id = $id;
         return $this;
     }
 
-    /**
-     * Get the value of title
-     */ 
+    // GET ET SET DU TITRE
     public function getTitle(){
         return $this->title;
-    }
-
-    /**
-     * Set the value of title
-     *
-     * @return  self
-     */ 
+    } 
     public function setTitle($title){
         $this->title = $title;
         return $this;
     }
 
-    /**
-     * Get the value of user
-     */ 
+    // GET ET SET DU USER
     public function getUser(){
         return $this->user;
     }
-
-    /**
-     * Set the value of user
-     *
-     * @return  self
-     */ 
     public function setUser($user){
         $this->user = $user;
         return $this;
     }
 
+    // METHODE TOOSTRING POUR L'AFFICHAGE
     public function __toString(){
         return $this->title;
     }
--- Forum_asma/model/entities/User.php
+++ Forum_asma/model/entities/User.php
@@ -4,7 +4,9 @@ namespace Model\Entities;
 use App\Entity;
 
 /*
-    En programmation orientée objet, une classe finale (final class) est une classe que vous ne pouvez pas étendre, c'est-à-dire qu'aucune autre classe ne peut hériter de cette classe. En d'autres termes, une classe finale ne peut pas être utilisée comme classe parente.
+    En programmation orientée objet, une classe finale (final class) est une classe que vous ne pouvez pas étendre, 
+    c'est-à-dire qu'aucune autre classe ne peut hériter de cette classe. 
+    En d'autres termes, une classe finale ne peut pas être utilisée comme classe parente.
 */
 
 final class User extends Entity{
@@ -12,45 +14,31 @@ final class User extends Entity{
     private $id;
     private $nickName;
 
+    // METHODE CONSTRUCT DE USER
     public function __construct($data){         
         $this->hydrate($data);        
     }
 
-    /**
-     * Get the value of id
-     */ 
+    // GET ET SET DE L'ID USER
     public function getId(){
         return $this->id;
     }
-
-    /**
-     * Set the value of id
-     *
-     * @return  self
-     */ 
     public function setId($id){
         $this->id = $id;
         return $this;
     }
 
-    /**
-     * Get the value of nickName
-     */ 
+    // GET ET SET DU PSEUDO USER
     public function getNickName(){
         return $this->nickName;
     }
-
-    /**
-     * Set the value of nickName
-     *
-     * @return  self
-     */ 
     public function setNickName($nickName){
         $this->nickName = $nickName;
 
         return $this;
     }
 
+    // METHODE TOOSTRING POUR L'AFFICHAGE
     public function __toString() {
         return $this->nickName;
     }
