fea91d14f83e917e7a2cd65bbf30215b9f1d2cfb
Asma SAIDI <saidiasma422@gmail.com>
2024-04-18 16:53:11 +0200

    add topic controller and the function index + the function listTopicsByCategory

---
+++ Forum_asma/controller/TopicController.php
@@ -0,0 +1,53 @@
+<?php
+
+    namespace Controller;
+
+    use App\Session;
+    use App\AbstractController;
+    use App\ControllerInterface;
+    use Model\Managers\TopicManager;
+    use Model\Managers\CategoryManager;
+    use Model\Managers\UserManager;
+
+    class TopicController extends AbstractController implements ControllerInterface{
+
+        public function index(){
+            
+        }
+        
+        public function listTopicsByCategory($id){
+
+            $topicManager = new TopicManager();
+            $categoryManager = new CategoryManager();
+            
+            $categorie = $categoryManager->findOneById($id);
+
+            $topics = $topicManager->topicsByCategory($id);
+
+            if($categorie){
+
+                if (isset($_POST['submitTopic'])){
+
+                header("Location: index.php?ctrl=topic&action=addTopic");
+                    
+                }
+            } else {
+                
+                $msg = "Cette catégorie n'existe pas !";
+                Session::addFlash('error', $msg);
+                
+                $this->redirectTo('forum');
+            }
+          
+                return [
+                        "view" => VIEW_DIR."forum/listTopics.php", //Interaction avec la vue
+                        "meta_description" => "Liste des Topics",
+                        "data" => [
+                        "topics" => $topics,
+                        "categorie" =>$categorie
+                        ]
+                ];
+
+
+        }
+    }
