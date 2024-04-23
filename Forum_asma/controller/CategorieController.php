919a7215db5d94cff793db842fa3da65ce03171e
SAIDI Asma <saidiasma422@gmail.com>
2024-04-23 09:20:57 +0200

    add comment into the categorieController

--- Forum_asma/controller/CategorieController.php
+++ Forum_asma/controller/CategorieController.php
@@ -20,7 +20,9 @@
                 "meta_description" => "Liste Catégories",
                 "view" => VIEW_DIR."forum/listCategories.php", //Comment le controller interagit avec la vue
                 "data" => [
-                    "category" =>  $categoryManager->findAll(["categoryName", "ASC"]) //la méthode "findAll" est une méthode générique qui provient de l'AbstractController (dont hérite chaque controller de l'application, il ne faut pas la modifier) Seuls sont à modifier les attributs dans le tableau en fonction des besoins.
+                    "category" =>  $categoryManager->findAll(["categoryName", "ASC"]) 
+                    //la méthode "findAll" est une méthode générique qui provient de l'AbstractController (dont hérite chaque controller de l'application, il ne faut pas la modifier)
+                    //Seuls sont à modifier les attributs dans le tableau en fonction des besoins.
                 ]
             ];   
         }
