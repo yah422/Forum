bd6f22c8cc3ac6a5be7da92bf05663c27263abee
SAIDI Asma <saidiasma422@gmail.com>
2024-04-24 11:17:07 +0200

    add registration function to the securityController

--- Forum_asma/controller/SecurityController.php
+++ Forum_asma/controller/SecurityController.php
@@ -11,8 +11,8 @@ class SecurityController extends AbstractController implements ControllerInterfa
         
     }
 
-    public function register () {
-        
+    public function registration(){
+            
         if(isset($_POST["submitRegistration"])){
 
             //on filtre les champs de saisie
@@ -21,8 +21,45 @@ class SecurityController extends AbstractController implements ControllerInterfa
             $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $confirmPassword = filter_input(INPUT_POST, "confirmPassword", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-            
+            if($username && $email && $password){
+
+                $userManager = new UserManager();
+
+                //on vérifie que l'utilisateur n'existe pas (mail)
+                if(!$userManager->findOneByEmail($email)){
+
+                    //on vérifie que le pseudo n'existe pas
+                     if(!$userManager->findOneByUser($username)){
+
+                        //on vérifie que les 2 passwords correspondent
+                        if($password == $confirmPassword){
+
+                            //on hash le password (password_hash)
+                            $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+                            // var_dump($passwordHash);
+                            //on ajoute l'user en bdd (pas besoin d'id car autoincrement)
+                            $userManager->add(["username" => $username, "email" =>$email, "password" => $passwordHash]);
+
+                            $msg = "Compte crée !";
+                            Session::addFlash('succés', $msg);
+
+                            //on redirige vers le formulaire de login dans la foulée
+                            $this->redirectTo('security', 'login');
+                        } else {                                
+                            
+                            $msg = "Mot de passe invalide !";
+                            Session::addFlash('erreur', $msg);
+
+                            $this->redirectTo('security', 'registration');
+                        }
+                    }
+                }
+            }
         }
+        return [
+            "metaDescription" => "Inscription",
+            "view" => VIEW_DIR."security/registration.php", //Interaction avec la vue
+        ];
     }
     
     public function login () {}
---
+++ Forum_asma/view/security/registration.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<div class="connexion">
+    <h1>Registration</h1>
+
+    <form enctype=mutlitpart/form-data action="index.php?ctrl=security&action=registration" method="post">
+        
+        <label for="username">Pseudonyme</label>
+        <input type="text" name="username" required>
+
+        <label for="email">Email</label>
+        <input type="email" name="email" required>
+
+        <label for="password">Mot De Passe</label>
+        <input type="password" name="password" required>
+
+        <label for="confirmPassword">Confirmation Mot De Passe</label>
+        <input type="password" name="confirmPassword" required>
+
+        <input type="submit" name="submitRegistration">
+
+    </form>
+
+    <p>Vous avez déjà un compte ? <a href="index.php?ctrl=security&action=login">Se connecter</a> !</p>
+</div>
