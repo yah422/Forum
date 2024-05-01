2f10a8a26c074e09478e57277c455a1ad758b6ad
Asma <saidiasma422@gmail.com>
2024-05-01 23:15:09 +0200

    add css and correct the add function to the postController

--- Forum_asma/controller/PostController.php
+++ Forum_asma/controller/PostController.php
@@ -40,24 +40,29 @@
         // ajout d'un post par l'id
         public function addPost($id){
             $postManager = new PostManager();
-        
             $userId = Session::getUser()->getId();
-        
+            
             if(isset($_POST['submitPost'])){
                 $content = filter_input(INPUT_POST, "content", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        
+                
                 if($content){
                     $postManager->add(["content" => $content, "topic_id" => $id, "user_id" => $userId]);
-                    $msg = "Post added";
+                    $msg = "Post ajouté";
                     Session::addFlash('success', $msg);
-                    header("Location: index.php?ctrl=post&action=listPostsByTopics&id=.'$id'.");
+                    header("Location: index.php?ctrl=post&action=listPostsByTopics&id=$id");
+                    exit; // Ajout pour arrêter l'exécution du script après la redirection
+                } else {
+                    $msg = "Le contenu du post est vide";
+                    Session::addFlash('error', $msg);
                 }
             }
-        
+            
+            // Cette partie du code sera toujours exécutée, même si la condition isset($_POST['submitPost']) est fausse
             return [
                 "view" => VIEW_DIR. "forum/listPosts.php"
             ];
         }
+        
         
         public function updatePost($id){
 
