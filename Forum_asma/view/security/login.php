9498ae693b805f51e39842496462cb151d38c461
Asma <saidiasma422@gmail.com>
2024-05-08 22:57:05 +0200

    add comments to the login part

--- Forum_asma/view/security/login.php
+++ Forum_asma/view/security/login.php
@@ -1,33 +1,41 @@
 <?php
+// Section de connexion au forum
 
 ?>
 
+<!-- Conteneur principal -->
 <div id="container">
-<div class="connexion">
-    <h1> CONNEXION AU FORUM </h1>
+    <!-- Formulaire de connexion -->
+    <div class="connexion">
+        <!-- Titre de la section -->
+        <h1> CONNEXION AU FORUM </h1>
 
-    <form id="connexionForm" enctype=mutlitpart/form-data action="index.php?ctrl=security&action=login" method="post">
+        <!-- Formulaire de connexion -->
+        <form id="connexionForm" enctype="multipart/form-data" action="index.php?ctrl=security&action=login" method="post">
 
-        <label class="labelConnect" for="email">Email</label>
-        <input class="inputConnexion" type="email" name="email" required>
-<br>
-        <label class="labelConnect" for="password">Mot De Passe</label>
-        <input class="inputConnexion" type="password" name="password" required>
-<br>
-<br>
-        <input id="inputSubmitInscription" type="submit" value="Se connecter" name="submitLogin">
+            <!-- Champ pour l'email -->
+            <label class="labelConnect" for="email">Email</label>
+            <input class="inputConnexion" type="email" name="email" required>
+            <br>
+            <!-- Champ pour le mot de passe -->
+            <label class="labelConnect" for="password">Mot De Passe</label>
+            <input class="inputConnexion" type="password" name="password" required>
+            <br>
+            <br>
+            <!-- Bouton de soumission du formulaire -->
+            <input id="inputSubmitInscription" type="submit" value="Se connecter" name="submitLogin">
 
-    </form>
+        </form>
 
-</div>  
+    </div>
 
-<div id="Connexion">
-    <a href="index.php?ctrl=security&action=registration">
-        <p>Vous n'avez encore un compte ? &nbsp; &nbsp;</p>
-        <p>  &nbsp; &nbsp; S'inscrire</p> 
-        
-    </a> 
-</div>
+    <!-- Lien vers la page d'inscription -->
+    <div id="Connexion">
+        <a href="index.php?ctrl=security&action=registration">
+            <p>Vous n'avez pas encore de compte ? &nbsp; &nbsp;</p>
+            <p>  &nbsp; &nbsp; S'inscrire</p> 
+        </a> 
+    </div>
 </div>
 <?php 
-    // var_dump($_SESSION["user"]);
+// var_dump($_SESSION["user"]);
