b899571a0e7fb8c56e00f7229a5e50f043a19d64
Asma <saidiasma422@gmail.com>
2024-05-08 22:55:56 +0200

    add comments to the updatePost

--- Forum_asma/view/forum/updatePost.php
+++ Forum_asma/view/forum/updatePost.php
@@ -1,29 +1,25 @@
 <?php
-//Modifier un post
-    $post = $result["data"]['post'];
-    // var_dump($post);
+// Récupération des données du post à modifier
+$post = $result["data"]['post'];
 ?>
+
+<!-- Section pour la modification du post -->
 <section id="wrapperUpdatePost">
+    <!-- Titre de la section -->
     <h3><a id="h3update"> MODIFICATION POST </a></h3>
 
-<!-- Nom du topic -->
-
-<!-- Question -->
-
-<!-- Afficher le contenu de l'ancien post -->
+    <!-- Affichage du contenu du post à modifier -->
     <div class="lastPost">
-
         <p><?= $post->getContent() ?></p>
-
     </div>
 
-        <form id="modifForm" action="index.php?ctrl=post&action=updatePost&id=<?=  $post->getId() ?>" method="post" enctype="multipart/form-data">
-
-            <label for="content">Content</label>
-
-            <textarea name="content" id="content"><?= $post->getContent() ?></textarea>
-            <br>
-            <input Value="Modifier" id="updateSubmit" href="index.php?ctrl=topic&action=updatePost&id=<?=  $post->getId() ?>" type="submit" name="submitUpdatePost">
-            
-        </form>
+    <!-- Formulaire de modification du post -->
+    <form id="modifForm" action="index.php?ctrl=post&action=updatePost&id=<?=  $post->getId() ?>" method="post" enctype="multipart/form-data">
+        <!-- Champ pour le nouveau contenu du post -->
+        <label for="content">Contenu</label>
+        <textarea name="content" id="content"><?= $post->getContent() ?></textarea>
+        <br>
+        <!-- Bouton de soumission pour la modification -->
+        <input value="Modifier" id="updateSubmit" href="index.php?ctrl=topic&action=updatePost&id=<?=  $post->getId() ?>" type="submit" name="submitUpdatePost">
+    </form>
 </section>
