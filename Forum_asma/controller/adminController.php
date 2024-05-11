3d64dacd1e0d19de70a03e9d412dfe2607d5451d
Asma <saidiasma422@gmail.com>
2024-05-11 22:34:37 +0200

    add comment to the adminController part

--- Forum_asma/controller/adminController.php
+++ Forum_asma/controller/adminController.php
@@ -13,15 +13,19 @@ class HomeController extends AbstractController implements ControllerInterface{
 
     }
 
-    //Accessible depuis le layout dans la section 'Users' réservée aux Admin
+    // Accessible depuis le layout dans la section 'Users' réservée aux administrateurs
     public function listUsers(){
 
+        // Restreint l'accès à cette fonctionnalité aux utilisateurs avec le rôle 'ROLE_ADMIN'
         $this->restrictTo("ROLE_ADMIN");
 
+        // Instancie le gestionnaire d'utilisateurs
         $userManager = new UserManager();
         
+        // Récupère tous les utilisateurs triés par date d'inscription décroissante
         $users =  $userManager->findAll(['registerDate', 'DESC']);
 
+        // Retourne les données nécessaires à la vue 'security/listUsers.php'
         return [
             "metaDescription" => "Liste Utilisateurs",
             "view" => VIEW_DIR."security/listUsers.php",
@@ -31,28 +35,38 @@ class HomeController extends AbstractController implements ControllerInterface{
         ];
     }
 
+    // Bannit un utilisateur
     public function ban($id){
 
+        // Instancie le gestionnaire d'utilisateurs
         $userManager = new UserManager();
 
+        // Bannit l'utilisateur
         $ban = $userManager->ban($id);
 
+        // Redirige vers la liste des utilisateurs
         $this->redirectTo('admin', 'listUsers');
 
+        // Retourne les données nécessaires à la vue 'security/listUsers.php'
         return [
             "metaDescription" => "Users List",
             "view" => VIEW_DIR."security/listUsers.php"
         ];
     }
-
+    
+    // Débannit un utilisateur
     public function deban($id){
 
+        // Instancie le gestionnaire d'utilisateurs
         $userManager = new UserManager();
 
+        // Débannit l'utilisateur
         $deban = $userManager->deban($id);
 
+        // Redirige vers la liste des utilisateurs
         $this->redirectTo('admin', 'listUsers');
         
+        // Retourne les données nécessaires à la vue 'security/listUsers.php'
         return [
             "metaDescription" => "Users List",
             "view" => VIEW_DIR."security/listUsers.php"
