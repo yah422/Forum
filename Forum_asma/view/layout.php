0126847c644ae6ca2435e743aff4a572fd627d56
Asma <saidiasma422@gmail.com>
2024-08-16 18:19:04 +0200

    add a

--- Forum_asma/view/layout.php
+++ Forum_asma/view/layout.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
         <!-- style CSS -->
         <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
+        <link rel="icon" href="public/images/discuter.png" type="image/png">
         <title>FORUM</title>
     </head>
     <body>
